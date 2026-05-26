e8d1357d804be73a2e384ba7234b1d5337142fd2
goopys9 <sgutierrez@unipacifico.edu.co>
2026-05-26 13:42:14 -0500

    Subiendo ordenamiento quicksort

---
+++ Unidad5/quicksort.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high]; // último elemento como pivote
+    int i = low;
+
+    for (int j = low; j < high; j++)
+    {
+        
+        if (arr[j] < pivot)
+        {
+            swap(&arr[i], &arr[j]);
+            i++;
+        }
+    }
+
+    swap(&arr[i], &arr[high]);
+
+    return i;
+}
+
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int p = partition(arr, low, high);
+
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
+    }
+}
+
+int main()
+{
+    int data[] = {6, 2, 4, 1, 5, 8, 12, 3, 9, 7};
+
+    int n = sizeof(data) / sizeof(data[0]);
+
+    
+    quickSort(data, 0, n - 1);
+
+    
+    printf("Arreglo ordenado de menor a mayor:\n");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", data[i]);
+    }
+
+    return 0;
+}
